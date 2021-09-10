33e4733a79198592d70a7eddd0a83a8a24b8fa67
Zulinlo <samm.wu2@gmail.com>
2021-09-10 21:40:09 +1000

    finished form for signup and login

--- client/src/components/entry/Login.js
+++ client/src/components/entry/Login.js
@@ -1,15 +1,33 @@
+import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+
 import "./styles.scss";
 
 const Login = () => {
   return (
-    <div className="container">
+    <section className="container">
       <div className="form">
         <h1>Welcome Back</h1>
-        <input type="text" />
-        <input type="password" />
+        <IconContext.Provider value={{ className: "input-placeholder-img" }}>
+          <div className="input-container">
+            <FaUserAlt />
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className="input-container">
+            <FaLock />
+            <input type="password" placeholder="Password" />
+          </div>
+        </IconContext.Provider>
         <button>Sign in</button>
+        <div className="link">
+          or{" "}
+          <Link to="/signup" style={{ textDecoration: "none" }}>
+            Sign Up
+          </Link>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- client/src/components/entry/SignUp.js
+++ client/src/components/entry/SignUp.js
@@ -1,11 +1,40 @@
+import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+
 import "./styles.scss";
 
 const SignUp = () => {
   return (
-    <>
-      <h1>Register An Account</h1>
-    </>
+    <section className="container">
+      <div className="background-pentagon-blue"></div>
+      <div className="background-triangle-red"></div>
+      <div className="form">
+        <h1>Register an Account</h1>
+        <IconContext.Provider value={{ className: "input-placeholder-img" }}>
+          <div className="input-container">
+            <FaUserAlt />
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className="input-container">
+            <FaLock />
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className="input-container">
+            <FaLock />
+            <input type="password" placeholder="Confirm Password" />
+          </div>
+        </IconContext.Provider>
+        <button>Sign up</button>
+        <div className="link">
+          or{" "}
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            Log in
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default SignUp
+export default SignUp;
